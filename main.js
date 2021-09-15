9a1677b59d7227164254ced9bc4c8447326d6af4
RohiniJ788 <72988330+RohiniJ788@users.noreply.github.com>
2021-09-15 20:19:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addUser(){
+    player1=document.getElementById("player_name_1").value;
+    player2=document.getElementById("player_name_2").value;
+
+    localStorage.setItem("player1",player1);
+    localStorage.setItem("player2",player2);
+
+    window.location="quiz_game_page.html";
+}
